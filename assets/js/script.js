0cef5428c0a735999b2e8658f88bfe795732a9d8
Odis Clemons <odis.clemons@gmail.com>
2022-04-16 13:28:02 -0400

    added comments to document.ready added variable to hold localStorage data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
 var searchCol = $("#search-col");
 var apiKey = "2bdae6894825c3874876330aedc1f2a6";
-var city = "Orlando" || "Search for a city..."
+var city = "" || "Search for a city..."
 var dt = moment().format("M/DD/YYYY")
 var cityNameDate = $('#city-name-date');
+var search = $('#inp-search')
 
-var cities = [
-  "Austin",
-  "Chicago",
-  "New York",
-  "Orlando",
-  "San Francisco",
-  "Seattle",
-  "Denver",
-];
+var weatherQuery
 
+// get whatever we have in localStorage 
+var storedCities = localStorage.getItem('wdbCities') 
+
+// if there is stuff there, stringify it or just set it to an empty array
+var cities = storedCities ? JSON.stringify(storedCities) : [];
+
+// when document is ready, do this stuff...
 $(()=>{
+  //loop through cities list and make a button for each one
   cities.map((city) => {
     let cityButton = `<button type="button" class="btn btn-sm btn-city">${city}</button>`;
     searchCol.append(cityButton);
   });
 
-  console.log(cityNameDate)
-
+  // set the city name and date at the top of page
   cityNameDate.text(`${city} (${dt})`)
 })
+
+function handleWeatherQuery(){
+  fetch()
+}
