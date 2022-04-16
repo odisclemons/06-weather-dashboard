e0af719057d6b236e19f1c4dfc2e1f041ee126c3
Odis Clemons <odis.clemons@gmail.com>
2022-04-16 13:44:21 -0400

    changed search div into form switched button for submit input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var searchCol = $("#search-col");
 var apiKey = "2bdae6894825c3874876330aedc1f2a6";
-var city = "" || "Search for a city..."
+var city = "" 
 var dt = moment().format("M/DD/YYYY")
 var cityNameDate = $('#city-name-date');
-var search = $('#inp-search')
+var inpSearch = $('#inp-search')
+var btnSearch = $('#btn-search')
+
 
 var weatherQuery
 
@@ -22,9 +24,18 @@ $(()=>{
   });
 
   // set the city name and date at the top of page
-  cityNameDate.text(`${city} (${dt})`)
+  cityNameDate.text(`${city || "Search for a city..."} (${dt})`)
+  btnSearch.click(handleWeatherQuery)
 })
 
-function handleWeatherQuery(){
-  fetch()
+function handleWeatherQuery(e){
+  e.preventDefault()
+  if(inpSearch[0].value) city = inpSearch[0].value
+  console.log(city)
+  if(!city) {
+    alert("Please specify a location first...")
+    return 
+  }
+  let query = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  fetch(query).then((res)=> res.json()).then(data => console.log(data))
 }
