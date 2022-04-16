748725a0ec60f29b974ff0cb4ceb11fddc795b3a
Odis Clemons <odis.clemons@gmail.com>
2022-04-16 15:04:51 -0400

    got weather icon displaying correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 var buttonContainer = $("#button-container");
 var apiKey = "2bdae6894825c3874876330aedc1f2a6";
 var city = "orlando"
-var dt = moment().format("M/DD/YYYY")
-var cityNameDate = $('#city-name-date');
+var curDt = moment().format("M/DD/YYYY")
+var cityName = $('#city-name')
+
+var city = ""
+var dt = $('#dt')
+var weatherIcon = $('#weather-icon')
+
 var inpSearch = $('#inp-search')
 var btnSearch = $('#btn-search')
 
@@ -22,9 +27,6 @@ $(() => {
   //make the buttons under search
   renderButtons()
 
-  // set the city name and date at the top of page
-  cityNameDate.text(`${city || "Search for a city..."} (${dt})`)
-
   //click handler for search button click
   btnSearch.click(handleWeatherQuery)
 })
@@ -42,7 +44,7 @@ function handleWeatherQuery(e) {
 
   //lets run the query and see what happens...
   fetch(query).then((res) => res.json()).then(data => {
-
+    console.log(data)
     //get the keys we need from it
     let { coord, weather, main, wind, name } = data
 
@@ -51,15 +53,20 @@ function handleWeatherQuery(e) {
     spnWind.text(`${wind.speed} MPH`)
     spnHumidity.text(`${main.humidity}%`)
     spnUv.text()
-    cityNameDate.text(`${city || "Search for a city..."} (${dt})`)
+
+    // set the city name and date at the top of page
+    cityName.text(city)
+    dt.text(`(${curDt})`)
+    weatherIcon.html(`<img src="http://openweathermap.org/img/wn/${weather[0].icon}.png" alt="weather icon" />`)
+
     updateLocalStorage(city)
   })
 }
 
 function updateLocalStorage(newCity) {
   // if this new city is not already on the list, add it to the top of the list 
-  console.log(cities, newCity, JSON.stringify([newCity, ...cities]))
   if (!cities.includes(newCity)) {
+    cities = [newCity, ...cities]
     localStorage.setItem('wdbCities', JSON.stringify([newCity, ...cities]))
     renderButtons()
   }
@@ -71,24 +78,23 @@ function updateLocalStorage(newCity) {
 function renderButtons() {
 
   console.log('render cities', cities)
-//first clear the buttons that are already there
-buttonContainer.html(null)
+  //first clear the buttons that are already there
+  buttonContainer.html(null)
 
-if(!cities ?? !cities.length){
-  buttonContainer.html('<h2>There are no recent cities yet...</h2>')
-  return
-}
+  if (!cities ?? !cities.length) {
+    buttonContainer.html('<h2>There are no recent cities yet...</h2>')
+    return
+  }
   //loop through cities list and make a button for each one
   cities.map((city) => {
-    console.log(city)
     let cityButton = `<button type="button" class="btn btn-sm btn-city" onclick="handleCityClick('${city}')">${city}</button>`;
     buttonContainer.append(cityButton);
   });
 }
 
-function handleCityClick(newCity){
+function handleCityClick(newCity) {
   console.log('newCity:', newCity)
   city = newCity
   inpSearch.value = newCity
-  handleWeatherQuery({ preventDefault: ()=>{}})
+  handleWeatherQuery({ preventDefault: () => { } })
 }
